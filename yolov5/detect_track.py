3d5a2f8787e254db9e9eb795677efeb3d3eb9499
Rishi Bhuptani <65431923+rishi-bhuptani@users.noreply.github.com>
2022-05-07 16:25:51 -0700

    Update detect_track.py
    commented

--- yolov5/detect_track.py
+++ yolov5/detect_track.py
@@ -215,11 +215,13 @@ def run(
                 #yolo write detection result 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
+                    '''
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    '''
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
